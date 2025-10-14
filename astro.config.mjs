3b0eb9831d6b4fbe8792c1d37d1967a9fc1e9015
Angel Angulo <angel.angulo95@gmail.com>
2025-10-14 18:04:57 +0200

    Adding google analitics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-
+import googleAnalytics from 'astro-google-analytics';
 import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [tailwind(), googleAnalytics({
+    measurementId: 'G-J9C85BJ5H4',
+  })],
   output: "server",
   adapter: vercel()
 });
