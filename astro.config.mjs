9d3ed528bc166e09953bf7b5b207136848a7c80f
Angel Angulo <angel.angulo95@gmail.com>
2025-10-25 13:04:23 +0200

    Adding native GA

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,11 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-import googleAnalytics from 'astro-google-analytics';
+
 import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), googleAnalytics({
-    measurementId: 'G-J9C85BJ5H4',
-  })],
+  integrations: [tailwind()],
   output: "server",
   adapter: vercel()
 });
